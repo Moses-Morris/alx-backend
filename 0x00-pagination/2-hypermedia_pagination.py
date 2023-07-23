a06739920ebf09b62ecb712e76bb08e65888676f
Moses-Morris <mosesmorrisdev@gmail.com>
2023-07-23 17:57:12 +0000

    Incognito

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-''' Hypermedia pagination '''
+"""
+"""
 import csv
 import math
-from typing import Dict, List, Tuple
+from typing import List, Dict
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    # """returns index range for items"""
+    return (((page - 1) * page_size), page * page_size)
 
 
 class Server:
@@ -25,36 +31,32 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        ''' def get page '''
-        assert type(page_size) is int and type(page) is int
-        assert page > 0
-        assert page_size > 0
-        self.dataset()
-        i = index_range(page, page_size)
-        if i[0] >= len(self.__dataset):
+        """returns a dataset for a given datarange"""
+        try:
+            assert (page > 0 or page_size > 0)
+            assert (type(page) is int or type(page_size) is int)
+            pages = index_range(page, page_size)
+            dataset = self.dataset()[pages[0]:pages[1]]
+            return dataset
+        except Exception:
             return []
-        else:
-            return self.__dataset[i[0]:i[1]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        ''' Def get hyper '''
-        dataset_items = len(self.dataset())
+        """returns a dict of meta data"""
         data = self.get_page(page, page_size)
-        total_pages = math.ceil(dataset_items / page_size)
+        if len(self.dataset()) % page_size == 0:
+            total_pages = len(self.dataset()) / page_size
+        else:
+            total_pages = len(self.dataset()) / page_size + 1
+        next_page = None if (page + 1) > total_pages else page + 1
+        prev_page = None if page - 1 < 0 else page - 1
 
-        p = {
-            "page": page,
-            "page_size": page_size if page < total_pages else 0,
-            "data": data,
-            "next_page": page + 1 if page + 1 < total_pages else None,
-            "prev_page": page - 1 if page - 1 > 0 else None,
-            "total_pages": total_pages
-            }
-        return p
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    ''' Def index range '''
-    index = page * page_size - page_size
-    index_1 = index + page_size
-    return (index, index_1)
+        my_dict = {
+            'page': page,
+            'page_size': page_size,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': int(total_pages)
+        }
+        return my_dict
